15a2d77a8d6780664775fb1f9e4fb615cc7b3378
yuvraj892 <yuvrajsrivastava892@gmail.com>
2025-01-27 15:19:20 +0530

    Added Task1 of question1

---
+++ fabonacii.cs
@@ -0,0 +1,17 @@
+//fabonaicii series 
+//Task1: print upto range 
+//Task2: Print n fabonacii number 
+using System;
+class Fabonacii{
+	static void Main(string []args){
+		Console.Write("Enter the number: ");
+		//taking the input from the user
+		int num=Convert.ToInt32(Console.ReadLine());
+		int sum=0;
+		//putting it in a loop so that numbers keep added in sum in order to gain fabonacii series 
+		for (int i=0;i<=num;i++){
+			sum +=i;
+		}
+		Console.WriteLine("sum is:" + sum);
+	}
+	}
